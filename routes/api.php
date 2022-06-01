4b18602cdd4d0b13d427b63df51ddf7a4f094d55
Md. Ashaduzzaman Faruque <Ashaduzzaman5698@gmail.com>
2022-06-01 22:15:40 +0600

    Auth Api Done

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[\App\Http\Controllers\Api\AuthController::class,'register']);
+Route::post('login',[\App\Http\Controllers\Api\AuthController::class,'login']);
+
+Route::group(["middleware" => ["auth:api"]], function(){
+
+    Route::get("profile", [\App\Http\Controllers\Api\AuthController::class, "profile"]);
+    Route::post("logout", [\App\Http\Controllers\Api\AuthController::class, "logout"]);
+});
